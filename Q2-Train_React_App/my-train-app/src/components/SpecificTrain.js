56ae441687a0eb4fb1631d26c88eaf2a504b0537
Yash Gangwar <gangwaryash06@gmail.com>
2023-08-22 21:46:37 +0530

    implemented page 2 of react app

--- Q2-Train_React_App/my-train-app/src/components/SpecificTrain.js
+++ Q2-Train_React_App/my-train-app/src/components/SpecificTrain.js
@@ -5,13 +5,38 @@ import './SpecificTrain.css'
 
 export default function SpecificTrain() {
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [trainInfo, setTrainInfo] = useState('');
+
+    const performSearch = () => {
+        try {
+            const res = fetch(`http://localhost:3000/train/trains/${searchTerm}`)
+            const result = res.json();
+            setTrainInfo(result)
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="body">
             <h2>Enter the train no. to get status:</h2>
             <div class="search-container">
-                <input type="text" class="search-input" placeholder="Search..."/>
-                <button class="search-button">Search</button>
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={event => setSearchTerm(event.target.value)}
+                />
+                <button className="search-button" onClick={performSearch}>Search</button>
             </div>
+            {trainInfo && (
+                <div>
+                    <h3>{trainInfo.trainName}</h3>
+                </div>
+            )}
         </div>
     );
 }
